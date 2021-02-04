8ac7f2927c3f21bd0d2dc1097d5c994128eeeff5
abakarovmaks <abakarovmaks@gmail.com>
2021-02-04 16:27:17 +0200

    Adds Button component

---
+++ my-app/src/Components/Button/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function Button({ route }) {
+  const navRoute = route ? route : '/';
+
+  return (
+    <button type="button">
+      <Link to={navRoute} style={{ textDecoration: 'none', color: 'black' }}>
+        {label}
+      </Link>
+    </button>
+  );
+}
+
+Button.propTypes = {
+  route: PropTypes.string,
+};

43dc1ace93ca5eae9dbc698f8b5b1f49252eb7bd
abakarovmaks <abakarovmaks@gmail.com>
2021-02-04 16:03:34 +0200

    Adds services file

---
+++ my-app/src/services/movies-api.js
@@ -0,0 +1,39 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const KEY = 'df130c075eac77942a31aeab2d405e14';
+
+async function fetchByURL(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function getTrendingData() {
+  const url = `${BASE_URL}/trending/movie/day?api_key=${KEY}&language=en-US`;
+  return fetchByURL(url);
+}
+function getSearchData(query) {
+  const url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&${query}`;
+  return fetchByURL(url);
+}
+function getMovieById(id) {
+  const url = `${BASE_URL}/movie/{movie_id}?api_key=${KEY}&language=en-US`;
+  return fetchByURL(url);
+}
+function getMovieCast(id) {
+  const url = `${BASE_URL}/movie/{movie_id}/credits?api_key=${KEY}&language=en-US`;
+  return fetchByURL(url);
+}
+function getMovieReviews(id) {
+  const url = `${BASE_URL}/movie/{movie_id}/reviews?api_key=${KEY}&language=en-US`;
+  return fetchByURL(url);
+}
+
+const api = {
+  getTrendingData,
+  getSearchData,
+  getMovieById,
+  getMovieCast,
+  getMovieReviews,
+};
+
+export default api;

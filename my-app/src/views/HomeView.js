6bb3ed33fbe5bb7ed3a4cbe9e9237376c19d3ee7
abakarovmaks <abakarovmaks@gmail.com>
2021-02-08 15:24:21 +0200

    Adds HomeView component

---
+++ my-app/src/views/HomeView.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import moviesAPI from '../services/movies-api';
+import MoviesPage from '../Components/MoviesPage/MoviesPage';
+import Loader from '../Components/Loader/Loader';
+import ErrorView from './ErrorView';
+
+export default function HomeView() {
+  const { isLoading, isError, isSuccess, data, error } = useQuery(
+    'trendingMovies',
+    moviesAPI.getTrendingData,
+  );
+
+  console.log(data);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {isSuccess && <MoviesPage movies={data.results} title="Trending today" />}
+      {isError && <ErrorView title={error.message} />}
+    </>
+  );
+}
